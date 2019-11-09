0bed2f56d0cd49eb81c432f169821c83a37f07ba
Valentin <valecart@student.42.fr>
2019-11-09 10:37:04 +0100

    profil page start

---
+++ profil.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Instapouet - Connexion</title>
+	<?php include('css-handler.php');?>
+</head>
+<body>
+<?php include("topmenu.php"); ?>
+    <?php
+        include("profil-header.php");
+    ?>
+<div class="main">
+<div class="container">
+</div>
+</div>
+</body>
+</html>
