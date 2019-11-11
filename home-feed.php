37df13e8b04d753e7df944a64c3f2d82e127f6e4
Valentin LECART <valecart@student.42.fr>
2019-11-11 18:21:24 +0100

    home feed started to be fill

--- home-feed.php
+++ home-feed.php
@@ -11,7 +11,8 @@
             </div><!--post-st end-->
         </div><!--post-topbar end-->
         <?php
-        for ($i = 0; $i < 5; $i++)
+        $images = get_images();
+        foreach ($images as $image)
             echo '<div class="posts-section">
             <div class="post-bar">
                 <div class="post_topbar">
@@ -31,14 +32,14 @@
                 </div>
                 
                 <div class="post_content">                    
-                    <a class="center" href="./image.php"><img class="post_img" src="https://picsum.photos/600" alt="sample image" \></a>
+                    <a class="center" href="./image.php?id=' . $image['img_ID'] .'"><img class="post_img" src="' . $image['img_path'] . '" alt="sample image" \></a>
                 </div>
                 <div class="post-status-bar">
                     <ul class="like-com">
                         <li>
-                            <a href="#"><i class="fas fa-heart"></i> Like ' . mt_rand(1,250) . '</a>
+                            <a href="#" onclick="like(' . $image['img_ID'] .')" onmouseover="chcl(\'#e44b4b\', \'heart\')" onmouseout="chcl(\'#b2b2b2\', \'heart\')"><i id="heart" class="fas fa-heart"></i> Like ' . mt_rand(1,250) . '</a>
                         </li> 
-                        <li><a href="./image.php#comments" class="com"><i class="fas fa-comment-alt"></i> Comment ' . mt_rand(1,75) . '</a></li>
+                        <li><a href="./image.php#comments" class="com" onmouseover="chcl(\'#4582EC\', \'com\')" onmouseout="chcl(\'#b2b2b2\', \'com\')"><i id="com" class="fas fa-comment"></i> Comment ' . mt_rand(1,75) . '</a></li>
                     </ul>
                 </div>
             </div><!--post-bar end-->
