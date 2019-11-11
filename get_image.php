dc5054b7a1ce0083b18b91b1b715e60861d106ca
Valentin LECART <valecart@student.42.fr>
2019-11-11 18:21:04 +0100

    get_images is working

--- get_image.php
+++ get_image.php
@@ -1,42 +1,49 @@
 <?php
-include("db_manager.php");
-try{
-    $bdd = new PDO($servername.";dbname=".$dbname, $username, $password);
+function db_connect(){
+    include("db_manager.php");
+    try {
+        $bdd = new PDO($servername.";dbname=".$dbname, $username, $password);
+    }
+    catch(Exception $e){
+        die('Erreur : '.$e->getMessage());
+    }
+    return $bdd;
 }
-catch(Exception $e){
-	die('Erreur : '.$e->getMessage());
-}
-$img_ID = $_POST['img_ID'];
-$user_ID = $_POST['user_ID'];
+// $img_ID = $_POST['img_ID'];
+// $user_ID = $_POST['user_ID'];
 //   $user_ID = 1;
 //     $img_ID = 1;
-
-if (isset($img_ID)){
+$rep = array();
+function get_image_by_ID($img_ID) {
+    $bdd = db_connect();
     $req = $bdd->prepare('SELECT * FROM `img`WHERE `img_ID`= ?');
     $req->execute(array($img_ID));
     
     while ($donnees = $req->fetch()){
-        echo $donnees['img_path']."\n";
+        $rep[] = $donnees;
     }
     $req->closeCursor();
+    return array_reverse($rep);
 }
-else if (isset($user_ID)){
+function get_image_by_user_ID($user_ID) {
+    $bdd = db_connect();
     $req = $bdd->prepare('SELECT * FROM `img`WHERE `user_ID`= ?');
     $req->execute(array($user_ID));
     
     while ($donnees = $req->fetch()){
-        echo $donnees['img_path']."\n";
+        $rep[] = $donnees;
     }
     $req->closeCursor();
-   
+    return array_reverse($rep);
 }
-else {
+function get_images() {
+    $bdd = db_connect();
     $req = $bdd->prepare('SELECT * FROM `img`');
     $req->execute(array());
     
     while ($donnees = $req->fetch()){
-        echo $donnees['img_path']."\n";
+        $rep[] = $donnees;
     }
     $req->closeCursor();
-
+    return array_reverse($rep);
 }
