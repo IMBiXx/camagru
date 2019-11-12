31f2e05a9a00c902d77fa57ba0d97fe47efff0c1
Valentin LECART <valecart@student.42.fr>
2019-11-12 12:11:57 +0100

    login improvement

--- login-core.php
+++ login-core.php
@@ -1,16 +1,25 @@
 <div class="col-lg-6 center">
     <form action="connexion.php" method="post">
-    <fieldset>
-        <div class="form-group">
-        <label>Adresse email</label>
-        <input type="email" name="mailconnect" class="form-control" aria-describedby="emailHelp" placeholder="Entrez votre adresse email">
-        </div>
-        <div class="form-group">
-        <label>Mot de passe</label>
-        <input type="password" name="mdpconnect" class="form-control" placeholder="Entrez votre mot de passe">
-        </div>
+        <fieldset>
+            <label>Adresse email</label>
+            <div class="form-group">
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="fas fa-envelope-open"></i></span>
+                    </div>
+                <input type="email" name="mailconnect" class="form-control" aria-describedby="emailHelp" placeholder="Entrez votre adresse email">
+                </div>
+            </div>
+            <label>Mot de passe</label>
+            <div class="form-group">
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="fa fa-lock"></i></span>
+                    </div>
+                <input type="password" name="mdpconnect" class="form-control" placeholder="Entrez votre mot de passe">
+                </div>
+            </div>
+            <button type="submit" name="formconnexion" class="btn btn-primary">Se connecter</button>
         </fieldset>
-        <button type="submit" name="formconnexion" class="btn btn-primary">Se connecter</button>
-    </fieldset>
     </form>
 </div>
