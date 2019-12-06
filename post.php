9b9ab1b1277442101e512c9ddaac2dccfa644fd0
Wafae RHARRABTI <warharra@e2r6p19.42.fr>
2019-12-06 09:54:57 +0100

    selctionner image est ok

--- post.php
+++ post.php
@@ -33,23 +33,21 @@
 session_start();
 date_default_timezone_set('europe/paris');
 include("db_manager.php");
-    try{
-            $bdd = new PDO($servername.";dbname=".$dbname, $username, $password);
-        } catch(PDOException $e){
-            die('Erreur:'.$e->getMessage());
-        }
-    $id = $_SESSION['id'];
-    if (isset($_POST['webcam']))
-        echo "<script type='text/javascript'>openWebcam();</script>";
-    else
-        $onload = "uploadImage()";
-    if (isset($_POST['capture']))
-     echo "<script type='text/javascript'>
-            context.drawImage(video,0,0,100,130);</script>";
+try{
+        $bdd = new PDO($servername.";dbname=".$dbname, $username, $password);
+} catch(PDOException $e){
+        die('Erreur:'.$e->getMessage());
+}
+$id = $_SESSION['id'];
+if (isset($_POST['webcam']))
+    echo "<script type='text/javascript'>openWebcam();</script>";
     
-        if($_SERVER["REQUEST_METHOD"] == "POST")
-        {
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+            
             if (isset($_POST['webcamuploaded'])) {
+                echo "je rentre";
+                
                 $data = $_POST['webcamuploaded'];
                 list($type, $data) = explode(';', $data);
                 list(, $data)      = explode(',', $data);
@@ -60,50 +58,32 @@ include("db_manager.php");
                     $i = $_SESSION['id1'] + 1;
                 $uploadfile = file_put_contents('./images/user_images/'. $i, $data);
                 $uploaddir = './images/user_images/'. $i;
-            
-            $iurl = $uploaddir;
-            $_SESSION['iurl'] = $iurl;
-        }
-        if (isset($_POST['poster_img']))
-        {
-            echo 'ulr: '.$_SESSION['iurl'];
-            $img_uploade_date = date("Y-m-d H:i:s");
-            $req = $bdd->prepare('INSERT INTO `img`(`user_ID`, `img_name`, `img_upload_date`, `img_path`) VALUES(?,?,?,?)');
-            $req->execute(array($id, "default", $img_uploade_date, $_SESSION['iurl']));
-            $iurl = $_SESSION['iurl'];
-            $req = $bdd->prepare('SELECT `img_ID` FROM `img` WHERE `img_path`= ?');
-            $req->execute(array($iurl)) && $img_id = $req->fetch();
-            $id1 = $img_id['img_ID'];
-            $_SESSION['iurl'] = 0;
-            $_SESSION['id1'] = $id1;
-            header("Location: ./image.php?id=".$id1);
-        }
-   }
-    
-    
-    
-    
-    if($_SERVER["REQUEST_METHOD"] == "POST"){ 
-        $uploaddir = './images/user_images/';
-        $uploadfile = $uploaddir . basename($_FILES['image']['name']);
-        if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile)) {
-            $iurl = $uploadfile;
-            $_SESSION['iurl'] = $uploadfile;
-        }
-        if (isset($_POST['image'])) {
-        if (isset($_POST['poster_img']))
-        {
-            echo 'ulr: '.$_SESSION['iurl'];
-            $img_uploade_date = date("Y-m-d H:i:s");
-            $req = $bdd->prepare('INSERT INTO `img`(`user_ID`, `img_name`, `img_upload_date`, `img_path`) VALUES(?,?,?,?)');
-            $req->execute(array($id, "default", $img_uploade_date, $_SESSION['iurl']));
-            $iurl = $_SESSION['iurl'];
-            $req = $bdd->prepare('SELECT `img_ID` FROM `img`WHERE `img_path`= ?');
-            $req->execute(array($iurl)) && $img_id = $req->fetch();
-            $id = $img_id['img_ID'];
-            $_SESSION['iurl'] = 0;
-            header("Location: ./image.php?id=".$id);
-        }
-}
+                $iurl = $uploaddir;
+                $_SESSION['iurl'] = $iurl;
+                
+            }
+            else
+            {
+                $uploaddir = './images/user_images/';
+                $uploadfile = $uploaddir . basename($_FILES['image']['name']);
+                if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile)) {
+                    $iurl = $uploadfile;
+                    $_SESSION['iurl'] = $iurl;
+                   
+                }
+           }
+    }
+if (isset($_POST['poster_img']))
+{
+    $img_uploade_date = date("Y-m-d H:i:s");
+    $req = $bdd->prepare('INSERT INTO `img`(`user_ID`, `img_name`, `img_upload_date`, `img_path`) VALUES(?,?,?,?)');
+    $req->execute(array($id, "default", $img_uploade_date, $_SESSION['iurl']));
+    $iurl = $_SESSION['iurl'];
+    $req = $bdd->prepare('SELECT `img_ID` FROM `img` WHERE `img_path`= ?');
+    $req->execute(array($iurl)) && $img_id = $req->fetch();
+    $id1 = $img_id['img_ID'];
+    $_SESSION['iurl'] = 0;
+    $_SESSION['id'] = $id;
+    header("Location: http://localhost:8080/cama/image.php?id=".$id);
 }
 ?>
