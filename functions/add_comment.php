a5e6f296790a15032908b0056b2870ca3ce78dea
Valentin LECART <valecart@student.42.fr>
2020-01-08 15:10:56 +0100

    fix errors

--- functions/add_comment.php
+++ functions/add_comment.php
@@ -1,4 +1,5 @@
 <?php
+include("update_user_preferences.php");
 function add_comment($user_ID, $img_ID, $comment_comment)
 {
     $comment_comment = htmlspecialchars($comment_comment);
@@ -10,14 +11,17 @@ function add_comment($user_ID, $img_ID, $comment_comment)
     $reqid = $bdd->prepare('SELECT `user_ID` FROM img WHERE `img_ID` = ?');
     $reqid->execute(array($img_ID)) && $row = $reqid->fetch();
     $user_id = $row['user_ID'];
-    $reqemail = $bdd->prepare('SELECT `user_email` FROM user WHERE `user_ID` = ?');
-    $reqemail->execute(array($user_id)) && $row = $reqemail->fetch();
-    $email = $row['user_email'];
-    $destinataire = $email;
-    $sujet = "Vous avez reçu un commentaire";
-    $headers .= 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    $headers .= 'From: "Camagru"<camagru@42.fr>' . "\n";
-    $message = 'Un utilisateur vient de commenter l\'une de vos photo';
-    mail($destinataire, $sujet, $message, $headers);
+    if (get_user_pref($user_id) == 1) {
+        $reqemail = $bdd->prepare('SELECT `user_email` FROM user WHERE `user_ID` = ?');
+        $reqemail->execute(array($user_id)) && $row = $reqemail->fetch();
+        $email = $row['user_email'];
+        $destinataire = $email;
+        $sujet = "Vous avez reçu un commentaire";
+        $headers .= 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        $headers .= 'From: "Camagru"<camagru@42.fr>' . "\n";
+        $message = 'Un utilisateur vient de commenter l\'une de vos photo';
+        mail($destinataire, $sujet, $message, $headers);
+    }
 }
+?>
